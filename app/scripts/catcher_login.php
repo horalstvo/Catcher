961e6b0f1b62ba32334d44d1c821dd4b2fd754d9
unknown <jiri.vosecek@gmail.com>
2013-04-07 13:32:51 +0200

    Přihlášení do aplikace bez databáze
    Deaktivace online přihlášení, aby nám to jelo i na serveru bez připojené databáze

--- app/scripts/catcher_login.php
+++ app/scripts/catcher_login.php
@@ -1,23 +1,26 @@
 <?php
-header('Content-Type: text/html; charset=utf-8');
-include "paths.php";
-include $path_conf."config.php";
-include $path_lib."dbconnect.php";
-include $path_lib."library.php";
+// header('content-type: text/html; charset=utf-8');
+// include "paths.php";
+// include $path_conf."config.php";
+// include $path_lib."dbconnect.php";
+// include $path_lib."library.php";
+// 
+// global $tournament, $password;
+// post2var("tournament,password");
 
-global $tournament, $password;
-post2var("tournament,password");
 
-
-$vysledek = mysql_query("SELECT * FROM mod_catcher_tournaments WHERE id = '$tournament'");
+// $vysledek = mysql_query("select * from mod_catcher_tournaments where id = '$tournament'");
 // zatím se s heslem nebudeme párat, přihlásíme se kamkoliv a kdykoliv 
-//  AND password = '$password'
+//  and password = '$password'
+
+$data["id"] = 15;
+$data["name"] = "Prague Four League (1/2013)";
 
 if(mysql_num_rows($vysledek) == 1){
 	$data = mysql_fetch_array($vysledek);
 	echo '{"success":true, "tournament_id":"'.$data["id"].'", "tournament_name":"'.$data["name"].'"}';
 }else{
-	echo '{"success":false,"message":"Chybné heslo"}';
+	echo '{"success":false,"message":"chybné heslo"}';
 }
 
 ?>
