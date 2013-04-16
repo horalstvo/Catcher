8bd660f2b98055724a6716148a77a4a97fa9b6bb
unknown <jiri.vosecek@gmail.com>
2013-04-16 21:26:26 +0200

    login skripty
    serverová záležitost

--- app/scripts/catcher_login.php
+++ app/scripts/catcher_login.php
@@ -1,26 +1,26 @@
 <?php
-// header('content-type: text/html; charset=utf-8');
-// include "paths.php";
-// include $path_conf."config.php";
-// include $path_lib."dbconnect.php";
-// include $path_lib."library.php";
-// 
-// global $tournament, $password;
-// post2var("tournament,password");
+header('content-type: text/html; charset=utf-8');
+include "paths.php";
+include $path_conf."config.php";
+include $path_lib."dbconnect.php";
+include $path_lib."library.php";
+
+global $tournament, $password;
+post2var("tournament,password");
 
 
-// $vysledek = mysql_query("select * from mod_catcher_tournaments where id = '$tournament'");
+$vysledek = mysql_query("select * from mod_catcher_tournaments where id = '$tournament'");
 // zatím se s heslem nebudeme párat, přihlásíme se kamkoliv a kdykoliv 
 //  and password = '$password'
 
-$data["id"] = 42;
-$data["name"] = "1. kvalifikace";
+// $data["id"] = 42;
+// $data["name"] = "1. kvalifikace";
 
-// if(mysql_num_rows($vysledek) == 1){
-// 	$data = mysql_fetch_array($vysledek);
+if(mysql_num_rows($vysledek) == 1){
+	$data = mysql_fetch_array($vysledek);
 	echo '{"success":true, "tournament_id":"'.$data["id"].'", "tournament_name":"'.$data["name"].'"}';
-// }else{
-// 	echo '{"success":false,"message":"chybné heslo"}';
-// }
+}else{
+	echo '{"success":false,"message":"chybné heslo či neexistují turnaj"}';
+}
 
 ?>
