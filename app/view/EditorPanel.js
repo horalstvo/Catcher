4b020ba6ade8bd2911cca9d24d750340b99df365
vosecek <jiri.vosecek@gmail.com>
2013-08-15 00:59:31 +0200

    Modální dialog, přidání zápasu

---
+++ app/view/EditorPanel.js
@@ -0,0 +1,98 @@
+Ext.define("catcher.view.EditorPanel",{
+	extend: "Ext.form.Panel",
+	xtype: "editorPanel",
+	id: "editorPanel",				
+	requires: ["Ext.form.FieldSet","Ext.form.Select","Ext.field.Hidden","Ext.field.Number"],
+  
+  config: {
+    title: "Úprava",
+    modal: true,
+    hideOnMaskTap: false,
+    styleHtmlContent: true,
+    centered: true,
+    width: "95%",
+    height:"95%",
+    maxHeight:"550px",
+    maxWidth:"550px",
+    
+    scrollable: true,        
+    items: [
+      {
+        xtype: 'fieldset',
+        items: [
+          {
+            xtype: 'selectfield',
+            name: 'home_id',
+            label: "První tým",
+            options:[]
+          },{
+            xtype: 'selectfield',
+            name: 'away_id',
+            label: "Druhý tým",
+            options:[]
+          },{
+						xtype: "selectfield",
+						label: "Hřiště",
+						name: "field",
+						options: []
+					},{
+						xtype: "numberfield",
+						label: "Délka utkání [min]",
+						name: "length",
+					},{
+						xtype: "datetimepickerfield",
+            dateTimeFormat: "j.n.Y H:i",
+						label: "Začátek dle rozpisu",
+						name: "time",
+						value: ""
+					},
+        ]
+      },{
+        docked: 'top',
+        xtype: 'toolbar',
+        title: 'Parametry utkání'
+      },{
+        docked: 'bottom',
+        ui: 'light',
+        xtype: 'toolbar',
+        items: [
+          {
+            ui: "decline",
+            text: 'Zrušit',
+            handler: function() {
+                Ext.getCmp('editorPanel').hide();
+            }
+          },{
+            xtype: 'spacer'
+          },{
+            text: 'Vytvořit',
+            ui: 'confirm',
+            handler: function() {
+              var formPanel = Ext.getCmp('editorPanel');
+              Ext.Viewport.setMasked({
+                xtype: "loadmask",
+                message: "Ukládám zápas na frisbee.cz"
+              });
+              var data = formPanel.getValues();
+              formPanel.hide();
+              var match = Ext.create("catcher.model.Match",data);                
+              var store = Ext.getCmp("matchesList").getStore(); 
+              store.add(match);                                
+              store.syncWithListener(function(){
+                store.load(function(){
+                  Ext.Viewport.setMasked(false);
+                  Ext.Msg.alert("OK","Zápas uložen");                  
+                });
+              });                              
+            }
+          }
+        ]
+      }
+    ],
+    listerners:{
+      painted:function(){
+        
+      }
+    }  
+  }  
+});
